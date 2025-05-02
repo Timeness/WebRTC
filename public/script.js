20d583698b84b3857a8e027f69a8221025d9be01
Timeness <careless@flash.co>
2025-05-02 21:13:55 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,10 +1,12 @@
 const roomId = window.location.pathname.split('/').pop();
-const socket = new WebSocket('ws://' + window.location.host);
+document.getElementById('roomId').innerText = roomId;
 
+const socket = new WebSocket('wss://' + window.location.host);
 let localStream, remoteStream, peerConnection;
 
 const localVideo = document.getElementById('localVideo');
 const remoteVideo = document.getElementById('remoteVideo');
+const endCallBtn = document.getElementById('endCallBtn');
 
 const config = {
   iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
@@ -16,29 +18,32 @@ socket.addEventListener('open', () => {
 });
 
 socket.addEventListener('message', async ({ data }) => {
-  const message = JSON.parse(data);
-
-  if (message.offer) {
+  const msg = JSON.parse(data);
+  if (msg.offer) {
     await createPeer();
-    await peerConnection.setRemoteDescription(new RTCSessionDescription(message.offer));
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(msg.offer));
     const answer = await peerConnection.createAnswer();
     await peerConnection.setLocalDescription(answer);
     socket.send(JSON.stringify({ answer }));
   }
-
-  if (message.answer) {
-    await peerConnection.setRemoteDescription(new RTCSessionDescription(message.answer));
+  if (msg.answer) {
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(msg.answer));
   }
-
-  if (message.candidate) {
-    await peerConnection.addIceCandidate(new RTCIceCandidate(message.candidate));
+  if (msg.candidate) {
+    await peerConnection.addIceCandidate(new RTCIceCandidate(msg.candidate));
   }
 });
 
 async function init() {
-  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  localStream = await navigator.mediaDevices.getUserMedia({
+    video: {
+      width: { ideal: 7680 },
+      height: { ideal: 4320 },
+      frameRate: { ideal: 60 }
+    },
+    audio: true
+  });
   localVideo.srcObject = localStream;
-
   if (!peerConnection) {
     await createPeer();
     const offer = await peerConnection.createOffer();
@@ -49,13 +54,11 @@ async function init() {
 
 async function createPeer() {
   peerConnection = new RTCPeerConnection(config);
-
   peerConnection.onicecandidate = e => {
     if (e.candidate) {
       socket.send(JSON.stringify({ candidate: e.candidate }));
     }
   };
-
   peerConnection.ontrack = e => {
     if (!remoteStream) {
       remoteStream = new MediaStream();
@@ -63,6 +66,13 @@ async function createPeer() {
     }
     remoteStream.addTrack(e.track);
   };
-
   localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
 }
+
+endCallBtn.onclick = () => {
+  if (peerConnection) peerConnection.close();
+  if (socket) socket.close();
+  localVideo.srcObject.getTracks().forEach(track => track.stop());
+  remoteVideo.srcObject = null;
+  alert('Call Ended');
+};
